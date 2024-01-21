9e91ccb1949f87b6293c38efb633be000df41799
Maksym Bezhevets <begik4@gmail.com>
2024-01-22 00:17:20 +0200

    Created new User serializer

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext as _
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
@@ -22,6 +22,16 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class UserUpdateForProfileSerializer(UserSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ("first_name", "last_name",)
+
 
 
 class AuthTokenSerializer(serializers.Serializer):
