3accaee67a19b90dd200fc1f0483e05309bfd830
Maksym Bezhevets <begik4@gmail.com>
2024-01-21 17:16:29 +0200

    TokenAuthentication is connected

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "social_media",
     "user",
 ]
@@ -129,3 +130,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
