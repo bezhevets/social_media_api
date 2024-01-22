b88dc0fab1990a5ab1bb08d3efd81286245c0610
Maksym Bezhevets <begik4@gmail.com>
2024-01-22 03:16:32 +0200

    Fixed bugs with fileds in userserializer.

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext as _
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
