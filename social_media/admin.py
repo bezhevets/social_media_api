1208ea60180775ecab4a73a03c91132683fed315
Maksym Bezhevets <begik4@gmail.com>
2024-01-22 16:07:13 +0200

    Created Post model, and added to admin panel.

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from social_media.models import Profile
+from social_media.models import Profile, Post
 
 # Register your models here.
 admin.site.register(Profile)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "owner",
+        "text",
+        "created_at",
+    ]
+    list_filter = ["owner"]
+    search_fields = ["owner"]
--- social_media/models.py
+++ social_media/models.py
@@ -23,10 +23,33 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
     )
     gender = models.CharField(max_length=15, choices=GenderChoices.choices)
-    bio = models.TextField(max_length=255, null=True,blank=True)
+    bio = models.TextField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=18, null=True, blank=True)
     image = models.ImageField(null=True, upload_to=profile_image_file_path)
     following = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="followers")
 
     def __str__(self):
         return str(self.owner.first_name + " " + self.owner.last_name)
+
+
+def post_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"id_post_{slugify(instance.id)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/posts/", filename)
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    text = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    hashtag = models.CharField(max_length=125, null=True, blank=True)
+    image = models.ImageField(null=True, upload_to=post_image_file_path, blank=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ["-created_at"]
