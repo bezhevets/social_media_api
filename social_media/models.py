f39faeeec843570636e663b4da75900893b87d51
Maksym Bezhevets <begik4@gmail.com>
2024-01-22 04:12:22 +0200

    Added field image, installed Pilow, and updated settings.

--- social_media/models.py
+++ social_media/models.py
@@ -1,5 +1,17 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
+
+
+def profile_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    full_name = instance.owner.first_name + " " + instance.owner.last_name
+    filename = f"{slugify(full_name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/profiles/", filename)
 
 
 class Profile(models.Model):
@@ -13,3 +25,7 @@ class Profile(models.Model):
     gender = models.CharField(max_length=15, choices=GenderChoices.choices)
     bio = models.TextField(max_length=255, null=True,blank=True)
     phone_number = models.CharField(max_length=18, null=True, blank=True)
+    image = models.ImageField(null=True, upload_to=profile_image_file_path)
+
+    def __str__(self):
+        return str(self.owner.first_name + " " + self.owner.last_name)
