2b0a04b280065a395fd72288e8cec9b73cd0a322
Maksym Bezhevets <begik4@gmail.com>
2024-01-23 14:57:39 +0200

    Updates method __str__ in Comment model.

--- social_media/models.py
+++ social_media/models.py
@@ -63,3 +63,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.owner.full_name} added comments to {self.post.id}"
